81d1fe8ade8308c307724be656708e297cb1b24f
raphael <raph.koh@gmail.com>
2020-10-12 15:59:21 -0400

    Remove drawer and unnecessary files

--- lib/screens/navigation/navigation_screen.dart
+++ lib/screens/navigation/navigation_screen.dart
@@ -1,12 +1,12 @@
-import 'package:arxiv_mobile/screens/feedback/feedback_screen.dart';
-import 'package:arxiv_mobile/screens/help/help_screen.dart';
+// import 'package:arxiv_mobile/screens/feedback/feedback_screen.dart';
+// import 'package:arxiv_mobile/screens/help/help_screen.dart';
 import 'package:arxiv_mobile/screens/home/home_screen.dart';
-import 'package:arxiv_mobile/screens/invite_friend/invite_friend_screen.dart';
+// import 'package:arxiv_mobile/screens/invite_friend/invite_friend_screen.dart';
 import 'package:arxiv_mobile/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 import 'components/drawer.dart';
-import 'components/drawer_user_controller.dart';
+// import 'components/drawer_user_controller.dart';
 
 class NavigationHomeScreen extends StatefulWidget {
   @override
@@ -33,37 +33,38 @@ class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
         bottom: false,
         child: Scaffold(
           backgroundColor: AppTheme.nearlyWhite,
-          body: DrawerUserController(
-            screenIndex: drawerIndex,
-            drawerWidth: MediaQuery.of(context).size.width * 0.75,
-            onDrawerCall: (DrawerIndex drawerIndexdata) => changeIndex(drawerIndexdata),
-            screenView: screenView,
-          ),
+          body: const MyHomePage(),
+          // body: DrawerUserController(
+          //   screenIndex: drawerIndex,
+          //   drawerWidth: MediaQuery.of(context).size.width * 0.75,
+          //   onDrawerCall: (DrawerIndex drawerIndexdata) => changeIndex(drawerIndexdata),
+          //   screenView: screenView,
+          // ),
         ),
       ),
     );
   }
 
-  void changeIndex(DrawerIndex drawerIndexdata) {
-    if (drawerIndex != drawerIndexdata) {
-      drawerIndex = drawerIndexdata;
-      if (drawerIndex == DrawerIndex.HOME) {
-        setState(() {
-          screenView = const MyHomePage();
-        });
-      } else if (drawerIndex == DrawerIndex.Help) {
-        setState(() {
-          screenView = HelpScreen();
-        });
-      } else if (drawerIndex == DrawerIndex.FeedBack) {
-        setState(() {
-          screenView = FeedbackScreen();
-        });
-      } else if (drawerIndex == DrawerIndex.Invite) {
-        setState(() {
-          screenView = InviteFriend();
-        });
-      }
-    }
-  }
+  // void changeIndex(DrawerIndex drawerIndexdata) {
+  //   if (drawerIndex != drawerIndexdata) {
+  //     drawerIndex = drawerIndexdata;
+  //     if (drawerIndex == DrawerIndex.HOME) {
+  //       setState(() {
+  //         screenView = const MyHomePage();
+  //       });
+  //     } else if (drawerIndex == DrawerIndex.Help) {
+  //       setState(() {
+  //         screenView = HelpScreen();
+  //       });
+  //     } else if (drawerIndex == DrawerIndex.FeedBack) {
+  //       setState(() {
+  //         screenView = FeedbackScreen();
+  //       });
+  //     } else if (drawerIndex == DrawerIndex.Invite) {
+  //       setState(() {
+  //         screenView = InviteFriend();
+  //       });
+  //     }
+  //   }
+  // }
 }
